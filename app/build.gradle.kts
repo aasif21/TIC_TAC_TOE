3a0b0180bc3ffd42bbb97883cb973190abc2fc0c
aasif <aasif.21ali@gmail.com>
2024-03-01 01:54:29 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tic_tac_toe"
-        minSdk = 21
+        minSdk = 25
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
